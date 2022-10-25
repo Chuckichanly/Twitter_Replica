8acf00cd3bf90ea912d2362aaafb882ac027f0be
Vitor Dilucca <vitordilucca@hotmail.com>
2022-10-25 17:07:08 -0300

    522 end Mostrando os tweets efetivamente na timeline, adicionado funçao para economizar linhas de code....

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -9,51 +9,47 @@ use MF\Model\Container;
 class AppController extends Action {
 
   public function timeline(){
+    
+    $this->validaAutenticacao();
+    
+    //recuperação dos tweets
+    $tweet = Container::getModel('Tweet');
 
-    session_start();
+    $tweet->__set('id_usuario', $_SESSION['id']);
 
-    if($_SESSION['id'] != '' && $_SESSION['nome'] != ''){
-      
-      //recuperação dos tweets
-      $tweet = Container::getModel('Tweet');
+    $tweets = $tweet->getAll();
 
-      $tweet->__set('id_usuario', $_SESSION['id']);
+    /* echo '<pre>';
+    print_r($tweets);
+    echo '</pre>'; */
 
-      $tweets = $tweet->getAll();
+    $this->view->tweets = $tweets;
 
-      echo '<pre>';
-      print_r($tweets);
-      echo '</pre>';
-
-      $this->view->tweets = $tweets;
-
-      $this->render('timeline');
-    }else{
-      header('Location: /?login=erro');
-    }
+    $this->render('timeline');
 
   }
 
   public function tweet(){
 
+    $this->validaAutenticacao();
+   
+    $tweet = Container::getModel('Tweet');
+    $tweet->__set('tweet', $_POST['tweet']);
+    $tweet->__set('id_usuario', $_SESSION['id']);
+    
+    $tweet->salvar();
+    header('Location: /timeline');
+
+  }
+  
+  public function validaAutenticacao(){
+
     session_start();
 
-    if($_SESSION['id'] != '' && $_SESSION['nome'] != ''){
-      
-      $tweet = Container::getModel('Tweet');
-//15.00
-      $tweet->__set('tweet', $_POST['tweet']);
-      $tweet->__set('id_usuario', $_SESSION['id']);
-      
-      $tweet->salvar();
-      header('Location: /timeline');
-
-    }else{
+    if(!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == ''){
       header('Location: /?login=erro');
     }
-    
   }
-
 }
 
 ?>
